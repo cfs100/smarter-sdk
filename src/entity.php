d95a5b5f28898e14de59118da846fa2aeb04947d
Caio Ferreira Silva <caio@ferreirasilva.com.br>
2018-01-04 19:16:55 -0200

    Creating entity class

---
+++ src/entity.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace smarter;
+
+class entity extends core
+{
+	const ENDPOINT = '/organizations/$organization$/entities';
+
+	public function get(array $params = [])
+	{
+		$aux = ['_dc' => time()];
+
+		$pagination = ['start', 'limit', 'page'];
+		foreach ($pagination as $field) {
+			$$field = isset($params[$field]) ? (integer) $params[$field] : null;
+		}
+
+		if ($limit) {
+			$aux['start'] = (integer) $start;
+			$aux['limit'] = $limit;
+			$aux['page'] = $page ?: 1;
+		}
+
+		if (isset($params['sort']) && ($sort = trim($params['sort']))) {
+			$aux['sort'] = $params['sort'];
+			$aux['dir'] = isset($params['dir']) ? $params['dir'] : 'ASC';
+		}
+
+		$url = $this->url($aux);
+
+		$curl = new curl($this, $url, curl::GET);
+		$curl->execute();
+
+		return $curl->result();
+	}
+
+	public function fullDetails($id)
+	{
+		$url = $this->url() . "/{$id}";
+
+		$curl = new curl($this, $url, curl::GET);
+		$curl->execute();
+
+		return $curl->response();
+	}
+
+	public function create(array $values)
+	{
+		$curl = new curl($this, $this->url(), curl::CREATE, ['entity' => $values]);
+
+		return [
+			'code' => $curl->execute(),
+			'response' => $curl->response(),
+		];
+	}
+}
