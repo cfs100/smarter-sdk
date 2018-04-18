d5de1ff8918174b376a6750275d2302251452891
Caio Ferreira Silva <caio@ferreirasilva.com.br>
2018-04-18 19:35:58 -0300

    Creating stock adjustment method

--- src/stock.php
+++ src/stock.php
@@ -57,4 +57,33 @@ class stock extends core
 			'response' => $curl->response(false),
 		];
 	}
+
+	public function adjustQuantity($stockID, $variation, $description)
+	{
+		$stockID = (integer) $stockID;
+		$url = $this->url() . "/{$stockID}/adjust_stock_balance";
+
+		$variation = (float) $variation;
+		$type = 10;
+
+		if ($variation < 0) {
+			$type = 11;
+			$variation *= -1;
+		}
+
+		$curl = new curl($this, $url, curl::EDIT, [
+			'adjustment' => [
+				'adjustment_type' => 1,
+				'movement_type' => $type,
+				'adjustment_quantity' => $variation,
+				'justification_manual_adjustment' => trim($description),
+			],
+			'company_id' => $this->expand('$company$'),
+		]);
+
+		return [
+			'code' => $curl->execute(),
+			'response' => $curl->response(false),
+		];
+	}
 }
