1160c4f048b780c1c7f3fbf7bcaccbab77bd78bd
Caio Ferreira Silva <caio@ferreirasilva.com.br>
2019-01-18 22:18:27 -0200

    Overriding outputNfe::get() with custom params

--- src/outputNfe.php
+++ src/outputNfe.php
@@ -23,4 +23,46 @@ class outputNfe extends core
 		self::STATUS_CANCELED => 'Cancelada',
 		self::STATUS_DUPLICATED => '"Duplicada',
 	];
+
+	/**
+	 * Queries Smarter using given parameters
+	 *
+	 * @param array $params
+	 * @return result
+	 */
+	public function get(array $params = [])
+	{
+		$aux = ['_dc' => time()];
+
+		$pagination = ['start', 'limit', 'page'];
+		foreach ($pagination as $field) {
+			$$field = isset($params[$field]) ? (integer) $params[$field] : null;
+		}
+
+		if ($limit) {
+			$aux['start'] = (integer) $start;
+			$aux['limit'] = $limit;
+			$aux['page'] = $page ?: 1;
+		}
+
+		if (isset($params['sort']) && ($sort = trim($params['sort']))) {
+			$aux['sort'] = $params['sort'];
+			$aux['dir'] = isset($params['dir']) ? $params['dir'] : 'ASC';
+		}
+
+		if (isset($params['filters']) && is_array($params['filters'])) {
+			$aux['filters'] = json_encode($params['filters']);
+		}
+
+		if (isset($params['is_grid'])) {
+			$aux['is_grid'] = $params['is_grid'] ? 'true' : 'false';
+		}
+
+		$url = $this->url($aux);
+
+		$curl = new curl($this, $url, curl::GET);
+		$curl->execute();
+
+		return $curl->result();
+	}
 }
